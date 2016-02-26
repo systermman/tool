9607ad8e5b04e168cc7c9d6aa9529c18d56b0869
yedegesong <277527478@qq.com>
2016-02-26 10:42:27 +0800

    新增去除重复数据

--- tool.js
+++ tool.js
@@ -14,9 +14,9 @@
             return this;
         },
         length: 0,
-        size: function() {
-        return this.length;
-    },
+        size: function () {
+            return this.length;
+        },
         constructor: Tool,
         versions: "1.1.0",
 
@@ -40,48 +40,48 @@
         return target;
     }
     //返回Tool 工具参数自带所有方法
-    Tool.key = function(obj){
+    Tool.key = function (obj) {
         var ObejctKey = {};
         var obj = obj ? obj : Tool;
-        for(p in obj){
-            ObejctKey[p] = obj[p];
+        for (key in obj) {
+            ObejctKey[key] = obj[key];
         }
         return ObejctKey;
     }
     Tool.each = function (objArray, funName) {
-            //功能: 用函数 funName 对数组 objArray 中的每个值进行处理一次，
-            for (var i = 0; i < objArray.length; i++) {
-                funName(i,objArray[i]);
-            }
+        //功能: 用函数 funName 对数组 objArray 中的每个值进行处理一次，
+        for (var i = 0; i < objArray.length; i++) {
+            funName(i, objArray[i]);
         }
+    }
     //Tool 类型检测
     Tool.extend({
-        type:function(e) {
-            if ( e == null ) {
-                return String( e );
+        type: function (e) {
+            if (e == null) {
+                return String(e);
             }
             return typeof e === "object" || typeof e === "function" ?
-            class2type[ toString.call(e) ] || "object" :
+            class2type[toString.call(e)] || "object" :
                 typeof e;
         },
         //检测一个对象是否包含某个属性
-        hasPrototype:function(object,name){
+        hasPrototype: function (object, name) {
             return object.hasOwnProperty ? object.hasOwnProperty(name) : (name in object);
         },
         //检测对象是否为空
-        isEmptyObject: function( obj ) {
+        isEmptyObject: function (obj) {
             var name;
-            for ( name in obj ) {
+            for (name in obj) {
                 return false;
             }
             return true;
-         },
+        },
         //判断是否是window对象
-        isWindow: function( obj ) {
+        isWindow: function (obj) {
             return obj != null && obj == obj.window;
         },
         //判断是否是一个函数
-        isFunction: function( obj ) {
+        isFunction: function (obj) {
             return jQuery.type(obj) === "function";
         },
         //判断是否是数组
@@ -101,27 +101,27 @@
             return e !== e;
         },
         //判断是否是纯碎的数字
-        isNumeric: function( obj ) {
-            return !isNaN( parseFloat(obj) ) && isFinite( obj );
+        isNumeric: function (obj) {
+            return !isNaN(parseFloat(obj)) && isFinite(obj);
         },
         //判断是否是微信
-        isWeiXin:function(){
+        isWeiXin: function () {
             return /MicroMessenger/i.test(navigator.userAgent);
         },
         //测试对象是否是纯粹的对象
-        isPlainObject: function( obj ) {
+        isPlainObject: function (obj) {
             console.log(obj.constructor)
             //如果参数是对象类型 不是WINDOW对象 没有构造函数
-            if(Tool.type(obj) === 'object'&&!Tool.isWindow(obj)&&obj.constructor){
+            if (Tool.type(obj) === 'object' && !Tool.isWindow(obj) && obj.constructor) {
                 return true;
             }
-                return false;
-            
+            return false;
+
         }
-        });
+    });
     //获取?后面所有参数
-    Tool.getQueryParas = function() {
-       var url = window.location.search;
+    Tool.getQueryParas = function () {
+        var url = window.location.search;
         var theRequest = new Object();
         if (url.indexOf("?") != -1) {
             var str = url.substr(1);
@@ -134,14 +134,14 @@
         return theRequest;
     }
     //返回?后面指定的参数
-    Tool.getQueryString = function(Paras){
+    Tool.getQueryString = function (Paras) {
         return Tool.getQueryParas()[Paras]
     }
     //数字不够自动补齐
-    Tool.fill=function (number, slice, fill, beginSlice) {
+    Tool.fill = function (number, slice, fill, beginSlice) {
         var fills = '';
         var slice = slice ? slice : 2;
-        var fill =  fill ? fill :'0';
+        var fill = fill ? fill : '0';
         var beginSlice = beginSlice ? beginSlice : 0;
         //如果传进来的数小于需要延伸的数
         if (String(number).length < slice) {
@@ -153,80 +153,94 @@
         fills += number;
         return beginSlice > 0 ? fills.slice(0, beginSlice) : fills.slice(-slice);
     }
-    Tool.each("Boolean Number String Function Array Date RegExp Object Error".split(" "), function(i, name) {
-        class2type[ "[object " + name + "]" ] = name.toLowerCase();
+    //去除重复数组
+    Tool.unique = function(arr) {
+        var res = [];
+        var json = {};
+        for(var i = 0; i < arr.length; i++){
+            //如果数组等于false,也就是为undefind 未定义的时候
+            if(!json[arr[i]]){
+                res.push(arr[i]);
+                json[arr[i]] = true;
+            }
+        }
+        return res;
+    }
+    Tool.each("Boolean Number String Function Array Date RegExp Object Error".split(" "), function (i, name) {
+        class2type["[object " + name + "]"] = name.toLowerCase();
     });
 
     Tool.fn.init.prototype = Tool.fn;
     window.Tool = T = Tool;
 })(window);
 //浏览器检测
-;(function($){
-  function detect(ua, platform){
-    var os = this.os = {}, browser = this.browser = {},
-      webkit = ua.match(/Web[kK]it[\/]{0,1}([\d.]+)/),
-      android = ua.match(/(Android);?[\s\/]+([\d.]+)?/),
-      osx = !!ua.match(/\(Macintosh\; Intel /),
-      ipad = ua.match(/(iPad).*OS\s([\d_]+)/),
-      ipod = ua.match(/(iPod)(.*OS\s([\d_]+))?/),
-      iphone = !ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/),
-      webos = ua.match(/(webOS|hpwOS)[\s\/]([\d.]+)/),
-      win = /Win\d{2}|Windows/.test(platform),
-      wp = ua.match(/Windows Phone ([\d.]+)/),
-      touchpad = webos && ua.match(/TouchPad/),
-      kindle = ua.match(/Kindle\/([\d.]+)/),
-      silk = ua.match(/Silk\/([\d._]+)/),
-      blackberry = ua.match(/(BlackBerry).*Version\/([\d.]+)/),
-      bb10 = ua.match(/(BB10).*Version\/([\d.]+)/),
-      rimtabletos = ua.match(/(RIM\sTablet\sOS)\s([\d.]+)/),
-      playbook = ua.match(/PlayBook/),
-      chrome = ua.match(/Chrome\/([\d.]+)/) || ua.match(/CriOS\/([\d.]+)/),
-      firefox = ua.match(/Firefox\/([\d.]+)/),
-      firefoxos = ua.match(/\((?:Mobile|Tablet); rv:([\d.]+)\).*Firefox\/[\d.]+/),
-      ie = ua.match(/MSIE\s([\d.]+)/) || ua.match(/Trident\/[\d](?=[^\?]+).*rv:([0-9.].)/),
-      webview = !chrome && ua.match(/(iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)/),
-      safari = webview || ua.match(/Version\/([\d.]+)([^S](Safari)|[^M]*(Mobile)[^S]*(Safari))/)
+;
+(function ($) {
+    function detect(ua, platform) {
+        var os = this.os = {}, browser = this.browser = {},
+            webkit = ua.match(/Web[kK]it[\/]{0,1}([\d.]+)/),
+            android = ua.match(/(Android);?[\s\/]+([\d.]+)?/),
+            osx = !!ua.match(/\(Macintosh\; Intel /),
+            ipad = ua.match(/(iPad).*OS\s([\d_]+)/),
+            ipod = ua.match(/(iPod)(.*OS\s([\d_]+))?/),
+            iphone = !ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/),
+            webos = ua.match(/(webOS|hpwOS)[\s\/]([\d.]+)/),
+            win = /Win\d{2}|Windows/.test(platform),
+            wp = ua.match(/Windows Phone ([\d.]+)/),
+            touchpad = webos && ua.match(/TouchPad/),
+            kindle = ua.match(/Kindle\/([\d.]+)/),
+            silk = ua.match(/Silk\/([\d._]+)/),
+            blackberry = ua.match(/(BlackBerry).*Version\/([\d.]+)/),
+            bb10 = ua.match(/(BB10).*Version\/([\d.]+)/),
+            rimtabletos = ua.match(/(RIM\sTablet\sOS)\s([\d.]+)/),
+            playbook = ua.match(/PlayBook/),
+            chrome = ua.match(/Chrome\/([\d.]+)/) || ua.match(/CriOS\/([\d.]+)/),
+            firefox = ua.match(/Firefox\/([\d.]+)/),
+            firefoxos = ua.match(/\((?:Mobile|Tablet); rv:([\d.]+)\).*Firefox\/[\d.]+/),
+            ie = ua.match(/MSIE\s([\d.]+)/) || ua.match(/Trident\/[\d](?=[^\?]+).*rv:([0-9.].)/),
+            webview = !chrome && ua.match(/(iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)/),
+            safari = webview || ua.match(/Version\/([\d.]+)([^S](Safari)|[^M]*(Mobile)[^S]*(Safari))/)
 
-    // Todo: clean this up with a better OS/browser seperation:
-    // - discern (more) between multiple browsers on android
-    // - decide if kindle fire in silk mode is android or not
-    // - Firefox on Android doesn't specify the Android version
-    // - possibly devide in os, device and browser hashes
-    if (browser.webkit = !!webkit) browser.version = webkit[1]
+        // Todo: clean this up with a better OS/browser seperation:
+        // - discern (more) between multiple browsers on android
+        // - decide if kindle fire in silk mode is android or not
+        // - Firefox on Android doesn't specify the Android version
+        // - possibly devide in os, device and browser hashes
+        if (browser.webkit = !!webkit) browser.version = webkit[1]
 
-    if (android) os.android = true, os.version = android[2]
-    if (iphone && !ipod) os.ios = os.iphone = true, os.version = iphone[2].replace(/_/g, '.')
-    if (ipad) os.ios = os.ipad = true, os.version = ipad[2].replace(/_/g, '.')
-    if (ipod) os.ios = os.ipod = true, os.version = ipod[3] ? ipod[3].replace(/_/g, '.') : null
-    if (wp) os.wp = true, os.version = wp[1]
-    if (webos) os.webos = true, os.version = webos[2]
-    if (touchpad) os.touchpad = true
-    if (blackberry) os.blackberry = true, os.version = blackberry[2]
-    if (bb10) os.bb10 = true, os.version = bb10[2]
-    if (rimtabletos) os.rimtabletos = true, os.version = rimtabletos[2]
-    if (playbook) browser.playbook = true
-    if (kindle) os.kindle = true, os.version = kindle[1]
-    if (silk) browser.silk = true, browser.version = silk[1]
-    if (!silk && os.android && ua.match(/Kindle Fire/)) browser.silk = true
-    if (chrome) browser.chrome = true, browser.version = chrome[1]
-    if (firefox) browser.firefox = true, browser.version = firefox[1]
-    if (firefoxos) os.firefoxos = true, os.version = firefoxos[1]
-    if (ie) browser.ie = true, browser.version = ie[1]
-    if (safari && (osx || os.ios || win)) {
-      browser.safari = true
-      if (!os.ios) browser.version = safari[1]
+        if (android) os.android = true, os.version = android[2]
+        if (iphone && !ipod) os.ios = os.iphone = true, os.version = iphone[2].replace(/_/g, '.')
+        if (ipad) os.ios = os.ipad = true, os.version = ipad[2].replace(/_/g, '.')
+        if (ipod) os.ios = os.ipod = true, os.version = ipod[3] ? ipod[3].replace(/_/g, '.') : null
+        if (wp) os.wp = true, os.version = wp[1]
+        if (webos) os.webos = true, os.version = webos[2]
+        if (touchpad) os.touchpad = true
+        if (blackberry) os.blackberry = true, os.version = blackberry[2]
+        if (bb10) os.bb10 = true, os.version = bb10[2]
+        if (rimtabletos) os.rimtabletos = true, os.version = rimtabletos[2]
+        if (playbook) browser.playbook = true
+        if (kindle) os.kindle = true, os.version = kindle[1]
+        if (silk) browser.silk = true, browser.version = silk[1]
+        if (!silk && os.android && ua.match(/Kindle Fire/)) browser.silk = true
+        if (chrome) browser.chrome = true, browser.version = chrome[1]
+        if (firefox) browser.firefox = true, browser.version = firefox[1]
+        if (firefoxos) os.firefoxos = true, os.version = firefoxos[1]
+        if (ie) browser.ie = true, browser.version = ie[1]
+        if (safari && (osx || os.ios || win)) {
+            browser.safari = true
+            if (!os.ios) browser.version = safari[1]
+        }
+        if (webview) browser.webview = true
+
+        os.tablet = !!(ipad || playbook || (android && !ua.match(/Mobile/)) ||
+        (firefox && ua.match(/Tablet/)) || (ie && !ua.match(/Phone/) && ua.match(/Touch/)))
+        os.phone = !!(!os.tablet && !os.ipod && (android || iphone || webos || blackberry || bb10 ||
+        (chrome && ua.match(/Android/)) || (chrome && ua.match(/CriOS\/([\d.]+)/)) ||
+        (firefox && ua.match(/Mobile/)) || (ie && ua.match(/Touch/))))
     }
-    if (webview) browser.webview = true
 
-    os.tablet = !!(ipad || playbook || (android && !ua.match(/Mobile/)) ||
-      (firefox && ua.match(/Tablet/)) || (ie && !ua.match(/Phone/) && ua.match(/Touch/)))
-    os.phone  = !!(!os.tablet && !os.ipod && (android || iphone || webos || blackberry || bb10 ||
-      (chrome && ua.match(/Android/)) || (chrome && ua.match(/CriOS\/([\d.]+)/)) ||
-      (firefox && ua.match(/Mobile/)) || (ie && ua.match(/Touch/))))
-  }
-
-  detect.call($, navigator.userAgent, navigator.platform)
-  // make available to unit tests
-  $.__detect = detect
+    detect.call($, navigator.userAgent, navigator.platform)
+    // make available to unit tests
+    $.__detect = detect
 
 })(Tool);
